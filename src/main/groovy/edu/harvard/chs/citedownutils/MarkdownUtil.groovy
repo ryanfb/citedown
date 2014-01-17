29f7c1ed120bda37442e32bdf512d847d913402d
Neel Smith <dnsmith.neel@gmail.com>
2014-01-17 17:52:58 -0500

    Add support for bullet lists.

--- src/main/groovy/edu/harvard/chs/citedownutils/MarkdownUtil.groovy
+++ src/main/groovy/edu/harvard/chs/citedownutils/MarkdownUtil.groovy
@@ -1,26 +1,8 @@
 package edu.harvard.chs.citedownutils
 
+import edu.harvard.chs.citedown.PegDownProcessor
+import edu.harvard.chs.citedown.Extensions
 import edu.harvard.chs.citedown.ast.RootNode
-
-
-import org.parboiled.BaseParser;
-import org.parboiled.Context;
-import org.parboiled.Rule;
-import org.parboiled.annotations.*;
-import org.parboiled.common.ArrayBuilder;
-import org.parboiled.common.ImmutableList;
-import org.parboiled.parserunners.ParseRunner;
-import org.parboiled.parserunners.ReportingParseRunner;
-import org.parboiled.support.ParsingResult;
-import org.parboiled.support.StringBuilderVar;
-import org.parboiled.support.StringVar;
-import org.parboiled.support.Var;
-
-import edu.harvard.chs.citedown.ast.*;
-import edu.harvard.chs.citedown.ast.SimpleNode.Type;
-
-import static org.parboiled.errors.ErrorUtils.printParseErrors;
-import static org.parboiled.common.StringUtils.repeat;
 
 /** Utilities for working with citedown source and converting
  * to vanilla markdown.
@@ -30,9 +12,65 @@ class MarkdownUtil {
   /** Root node of pegdown parsing result. */
   RootNode root
 
+  /** Source text in citedown format. */
+  String citedown
+
   /** Constructor requiring root node. */
-  MarkdownUtil(RootNode rootNode) {
-    this.root = rootNode
+  MarkdownUtil(String citedownSource) {
+    this.citedown = citedownSource
+    PegDownProcessor pdp = new PegDownProcessor(Extensions.CITE)
+    this.root = pdp.parser.parse(citedownSource.toCharArray())
+  }
+
+  String toMarkdown() {
+    return toMarkdown(root, "", "")
+  }
+
+
+
+  String toMarkdown(Object n, String accumulated, String contextNote) {
+
+    String txt = ""
+    Integer starthere = n.getStartIndex()
+    Integer endhere = n.getEndIndex()
+
+    switch (n.getClass().name) {
+
+    case "edu.harvard.chs.citedown.ast.BulletListNode":
+    System.err.println "BULL LIST: " + citedown.substring(starthere,endhere)
+    contextNote = "BULLLIST"
+    break
+
+
+    case "edu.harvard.chs.citedown.ast.ListItemNode":
+    if (contextNote == "BULLLIST") {
+      txt = "-"
+    }
+    contextNote = ""
+    System.err.println "LIST ITEM: " + citedown.substring(starthere,endhere)
+    break
+
+
+    case "edu.harvard.chs.citedown.ast.SuperNode":
+    txt = "${citedown.substring(starthere,endhere).replaceAll(/\t/,'')}" + "\n"
+    contextNote = ""
+    break
+
+    case "edu.harvard.chs.citedown.ast.HeaderNode":
+    txt = "${citedown.substring(starthere,endhere).replaceAll(/\t/,'')}\n"
+    contextNote = ""
+    break
+    
+    default:
+    break
+    }
+    accumulated += txt
+
+
+    n.getChildren().each { c ->
+      accumulated =  toMarkdown(c, accumulated, contextNote)
+    }
+    return accumulated.toString()
   }
 
 }
---
+++ src/test/groovy/edu/harvard/chs/citedownutils/TestBullets.groovy
@@ -0,0 +1,20 @@
+package edu.harvard.chs.citedownutils
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+class TestBullets {
+
+  String markdownText = "Text paragraph\n\n- i 1\n- no 2\n- item 3\n\n"
+
+  @Test void testConstructor() {
+    MarkdownUtil mdu = new MarkdownUtil(markdownText)
+    assert mdu
+
+    String converted = mdu.toMarkdown()
+    def lines = converted.readLines()
+    assert lines[1] == "-i 1"
+
+  }
+  
+}

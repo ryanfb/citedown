75c71d17f7ac104758b4664dae78a97b1b6aba0d
Neel Smith <dnsmith.neel@gmail.com>
2014-01-18 21:07:58 -0500

    Add test on mixed content in bullet lists.

--- src/test/groovy/edu/harvard/chs/citedownutils/TestBullets.groovy
+++ src/test/groovy/edu/harvard/chs/citedownutils/TestBullets.groovy
@@ -6,8 +6,9 @@ import org.junit.Test
 class TestBullets {
 
   String markdownText = "Text paragraph\n\n- item 1\n- no 2\n- item 3\n\n"
+  String mixedText = "Text paragraph\n\n- *item* 1\n- no 2\n- item 3\n\n"
 
-  @Test void testConstructor() {
+  @Test void testSimple() {
     MarkdownUtil mdu = new MarkdownUtil(markdownText)
     assert mdu
 
@@ -15,7 +16,16 @@ class TestBullets {
     def lines = converted.readLines()
     System.err.println "Lines:  " + lines
     assert lines[1] == "- item 1"
-
   }
-  
+
+
+  @Test void testMixed() {
+    MarkdownUtil mdu = new MarkdownUtil(mixedText)
+    assert mdu
+
+    String converted = mdu.toMarkdown()
+    def lines = converted.readLines()
+    System.err.println "Lines:  " + lines
+    assert lines[1] == "- *item* 1"
+  }  
 }
